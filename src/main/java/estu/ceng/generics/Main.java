58e81ac850f1dad2d4f80164a70a7fce75baf7fe
Mustafa Emirhan Özer <emirhann0077@gmail.com>
2025-11-03 21:15:14 +0300

    Stack sınıfına yeni metod eklendi

--- src/main/java/estu/ceng/generics/Main.java
+++ src/main/java/estu/ceng/generics/Main.java
@@ -6,22 +6,20 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        // --- Stack Demo ---
         Stack<String> stack = new Stack<>();
         stack.push("Java");
         stack.push("Generics");
         stack.push("Lab");
 
-        System.out.println("Stack boyutu: " + stack.size());
-        System.out.println("En üstteki eleman (peek): " + stack.peek());
-        System.out.println("Pop ile çıkarılan: " + stack.pop());
-        System.out.println("Stack içeriği: " + stack);
+        System.out.println("Stack size: " + stack.size());
+        System.out.println("Top element (peek): " + stack.peek());
+        System.out.println("Popped element: " + stack.pop());
+        System.out.println("Stack contents: " + stack);
 
-        // --- MergeUtil Demo ---
         List<Integer> list1 = Arrays.asList(1, 3, 5);
         List<Integer> list2 = Arrays.asList(2, 4, 6, 7);
 
         List<Integer> merged = MergeUtil.merge(list1, list2);
-        System.out.println("Birleştirilmiş liste: " + merged);
+        System.out.println("Merged list: " + merged);
     }
 }
